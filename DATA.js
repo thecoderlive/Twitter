d966c30b9ed74a7b6025819ee0a01b911318e1bb
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-03-16 12:12:18 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "fa93b10d-ceb1-4a44-904c-a8fcd9dd152a",
+    "home_feed": [
+        {
+            "id": "f737a205-a7b4-45f0-ad89-26d4dbcc7885",
+            "artist_name": "myself",
+            "home_carousel": [
+                {
+                    "id": "93cca137-02cb-44a8-abfe-3bd9c6b44ded",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/260.png"
+                },
+                {
+                    "id": "9d69bf21-aa4b-48db-a808-21d7bc42fd3f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/262.png"
+                },
+                {
+                    "id": "99b39958-a005-4ee8-b15c-f3ed7a4db776",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/1.png"
+                },
+                {
+                    "id": "819660b5-43f5-4f31-affe-a77d78040c32",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/87.png"
+                },
+                {
+                    "id": "8ce0ae71-de23-4adf-a0df-e72a0531b199",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/211.png"
+                }
+            ],
+            "art_caption": "relate",
+            "art_description": "Fact head building appear send. Measure prepare ability sister staff describe. Else thousand mention center."
+        },
+        {
+            "id": "43fb7438-be35-4fc8-afe3-aac001a7c2cb",
+            "artist_name": "act",
+            "home_carousel": [
+                {
+                    "id": "c9de80f5-b63c-42b7-ba77-83e46af2f6d2",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/110.png"
+                },
+                {
+                    "id": "87b9c432-3f57-425c-a84b-4dbfb88be62a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/47.png"
+                },
+                {
+                    "id": "693cc05f-f8d0-4ddc-9e05-e0174a9109b2",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/222.png"
+                },
+                {
+                    "id": "d1bbacb2-c486-4a4e-9930-28702b06233d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/316.png"
+                },
+                {
+                    "id": "c5a33061-9c60-486b-aa17-7bae9e85858f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/185.png"
+                }
+            ],
+            "art_caption": "left",
+            "art_description": "National young her season fear. There the none race office rule leave. Right last when."
+        },
+        {
+            "id": "8e625f28-a4a7-46c1-a482-ad5d19b137fc",
+            "artist_name": "about",
+            "home_carousel": [
+                {
+                    "id": "cacf6c71-0a22-43e2-8199-ff330a8d29e1",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/247.png"
+                },
+                {
+                    "id": "13558735-a6cf-40b0-940a-37f28d16df72",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/205.png"
+                },
+                {
+                    "id": "5264809a-bdf5-444f-b609-4a3778a52526",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/40.png"
+                },
+                {
+                    "id": "414bc590-a256-4914-a88d-ce69966f1e6e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/122.png"
+                },
+                {
+                    "id": "a93ec781-cec1-4be8-9739-1cf24d1bd7bb",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/126.png"
+                }
+            ],
+            "art_caption": "special",
+            "art_description": "Rise above throughout fear goal official event."
+        },
+        {
+            "id": "0ae54857-9702-487b-a296-2260f204cdbf",
+            "artist_name": "thank",
+            "home_carousel": [
+                {
+                    "id": "82cceaff-28ce-4ed3-8a6f-f27fae79dbf6",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/308.png"
+                },
+                {
+                    "id": "fc646790-c49d-44f7-a0d7-c4a5e0416ee4",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/337.png"
+                },
+                {
+                    "id": "8d55cdb0-3cf1-4f21-aa47-e7a57434b70f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/284.png"
+                },
+                {
+                    "id": "b22c5edb-9f0d-43f6-bb5f-159c8fc5a453",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/44.png"
+                },
+                {
+                    "id": "39231bc1-dd0c-4b52-a1b8-90f629be5a8a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/326.png"
+                }
+            ],
+            "art_caption": "sister",
+            "art_description": "Structure head win mean gun site peace. Floor act eat hit. Treat keep region yes artist mission."
+        },
+        {
+            "id": "b9ed33d6-970f-4450-b248-8f34904b9192",
+            "artist_name": "good",
+            "home_carousel": [
+                {
+                    "id": "2d68a6e4-a4b1-4399-9f9f-cbb52711866c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/157.png"
+                },
+                {
+                    "id": "869a0a24-cc4a-4319-ad99-287da4f23b73",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/112.png"
+                },
+                {
+                    "id": "85fd7398-5fd6-4a36-b29f-717bf8f517aa",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/148.png"
+                },
+                {
+                    "id": "de18dfe5-d715-4a52-b0e0-1c08a1fa4834",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/20.png"
+                },
+                {
+                    "id": "3cbceee8-7b88-4f8b-8282-5d71c6ba28d4",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/272.png"
+                }
+            ],
+            "art_caption": "raise",
+            "art_description": "Process small child final. Data they benefit. Card a trade where draw marriage."
+        }
+    ]
+}
